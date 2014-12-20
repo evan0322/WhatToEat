44268cca9ddb7dbe6f5b44c7ed2bd86dd1fa5c23
WEI XIE <weixie@WEIs-MacBook-Pro.local>
2014-12-20 11:25:29 -0500

    add controller to add food controller

---
+++ WhatToEat/controllers/AddFoodTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddFoodTableViewController.h
+//  WhatToEat
+//
+//  Created by WEI XIE on 2014-12-20.
+//  Copyright (c) 2014 WEI.XIE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddFoodTableViewController : UITableViewController
+
+@end
