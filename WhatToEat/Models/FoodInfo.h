763e6737769932587c18b43f220da83f07790b92
WEI XIE <weixie@WEIs-MacBook-Pro.local>
2015-02-16 15:44:13 -0500

    Add picture to the app

---
+++ WhatToEat/Models/FoodInfo.h
@@ -0,0 +1,17 @@
+//
+//  FoodInfo.h
+//  WhatToEat
+//
+//  Created by Wei Xie on 2014-12-22.
+//  Copyright (c) 2014 WEI.XIE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface FoodInfo : NSManagedObject
+
+@property (nonatomic, retain) NSString * name;
+
+@end
